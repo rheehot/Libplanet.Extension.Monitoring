4a621b0621b6f2cc1629a74ef8162ae13459909d
moreal <dev.moreal@gmail.com>
2019-12-19 03:58:43 +0900

    Implements Agent

--- Libplanet.State.Explorer/Agent.cs
+++ Libplanet.State.Explorer/Agent.cs
@@ -1,28 +1,68 @@
 ﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
 using System.Threading.Tasks;
+using Bencodex;
 using Libplanet.Action;
 using Libplanet.Blockchain;
+using Libplanet.Store;
 using NetMQ;
+using NetMQ.Monitoring;
+using NetMQ.Sockets;
+using Serilog;
 
 namespace Libplanet.State.Explorer
 {
     public class Agent<T>
         where T : IAction, new()
     {
-        private NetMQPoller _poller;
-        private NetMQQueue<>
+        private IStore _store;
+        private BlockChain<T> _chain;
+
+        private PublisherSocket _publisher;
+        private NetMQMonitor _monitor;
+
         private void TipChangedHandler(object o, BlockChain<T>.TipChangedEventArgs e)
         {
+            var blockHash = e.Hash;
+            var updatedAddresses = _chain[blockHash].Transactions
+                .SelectMany(tx => tx.UpdatedAddresses)
+                .ToImmutableHashSet();
+
+            var codec = new Codec();
+            foreach (var address in updatedAddresses)
+            {
+                var state = _chain.GetState(address, blockHash);
+                _publisher
+                    .SendMoreFrame(address.ToByteArray())
+                    .SendMoreFrame(NetworkOrderBitsConverter.GetBytes(e.Index))
+                    .SendMoreFrame(e.Hash.ToByteArray())
+                    .SendMoreFrame(e.PreviousHash?.ToByteArray() ?? new byte[0])
+                    .SendFrame(codec.Encode(state));
+            }
         }
 
-        public Agent(BlockChain<T> blockChain, string host, short port)
+        public Agent(BlockChain<T> blockChain, IStore store)
         {
-            blockChain.TipChanged += TipChangedHandler;
+            _store = store ?? throw new ArgumentNullException(nameof(store));
+
+            _chain = blockChain ?? throw new ArgumentNullException(nameof(blockChain));
+            _chain.TipChanged += TipChangedHandler;
         }
 
-        public Task StartAsync()
+        public Task StartAsync(ushort port)
         {
-            
+            _publisher = new PublisherSocket($"tcp://*:{port}");
+            _monitor = new NetMQMonitor(_publisher, "inproc://pub.inproc", SocketEvents.Accepted);
+            _monitor.Accepted += (sender, args) => { Console.WriteLine(args.Address); };
+            return _monitor.StartAsync();
+        }
+
+        public void Dispose()
+        {
+            _chain.TipChanged -= TipChangedHandler;
+            _publisher?.Close();
+            _monitor?.Stop();
         }
     }
 }
