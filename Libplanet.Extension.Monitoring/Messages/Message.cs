f9a645289bd7e97d4f7cd89ea8f524708de0464c
moreal <dev.moreal@gmail.com>
2020-01-19 22:30:34 +0900

    Implement missing method in Message

--- Libplanet.Extension.Monitoring/Messages/Message.cs
+++ Libplanet.Extension.Monitoring/Messages/Message.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Libplanet.Net;
 using NetMQ;
 
 namespace Libplanet.Extension.Monitoring.Messages
@@ -37,6 +41,37 @@ namespace Libplanet.Extension.Monitoring.Messages
         protected abstract MessageType Type { get; }
 
         protected abstract IEnumerable<NetMQFrame> DataFrames { get; }
+
+        private static readonly ImmutableDictionary<MessageType, Type> TypeMapping =
+            ImmutableDictionary<MessageType, Type>.Empty
+                .Add(MessageType.GetTip, typeof(GetTip))
+                .Add(MessageType.Tip, typeof(Tip))
+                .Add(MessageType.GetBlock, typeof(GetBlock))
+                .Add(MessageType.Block, typeof(Block))
+                .Add(MessageType.GetBlockHash, typeof(GetBlockHash))
+                .Add(MessageType.GetState, typeof(GetState))
+                .Add(MessageType.State, typeof(State));
+
+        public static Message Parse(NetMQMessage raw)
+        {
+            if (raw.FrameCount == 0)
+            {
+                throw new ArgumentException(
+                    $"Can't parse {nameof(Message)}, " +
+                    $"because ${nameof(raw)} was empty.",
+                    nameof(raw));
+            }
+
+            var rawType = (MessageType)raw[0].ConvertToInt32();
+            if (!TypeMapping.TryGetValue(rawType, out Type type))
+            {
+                throw new InvalidMessageException(
+                    $"Unknown message came. [{nameof(rawType)} == {rawType}]");
+            }
+
+            var bodyFrames = raw.Skip(1).ToArray();
+            return (Message)Activator.CreateInstance(type, new[] {bodyFrames});
+        }
 
         public NetMQMessage ToNetMQMessage()
         {
