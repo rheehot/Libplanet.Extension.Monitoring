741ab24be5a9e3fb930dd81a680a7b082f5c0521
moreal <dev.moreal@gmail.com>
2020-01-19 22:57:27 +0900

    Implement processing messages

--- Libplanet.Extension.Monitoring/Agent.cs
+++ Libplanet.Extension.Monitoring/Agent.cs
@@ -1,15 +1,12 @@
 ﻿using System;
-using System.Collections.Immutable;
-using System.Linq;
-using System.Threading.Tasks;
 using Bencodex;
 using Libplanet.Action;
 using Libplanet.Blockchain;
+using Libplanet.Extension.Monitoring.Messages;
+using Libplanet.Net;
 using Libplanet.Store;
 using NetMQ;
-using NetMQ.Monitoring;
 using NetMQ.Sockets;
-using Serilog;
 
 namespace Libplanet.Extension.Monitoring
 {
@@ -19,50 +16,65 @@ namespace Libplanet.Extension.Monitoring
         private IStore _store;
         private BlockChain<T> _chain;
 
-        private PublisherSocket _publisher;
-        private NetMQMonitor _monitor;
-
-        private void TipChangedHandler(object o, BlockChain<T>.TipChangedEventArgs e)
-        {
-            var blockHash = e.Hash;
-            var updatedAddresses = _chain[blockHash].Transactions
-                .SelectMany(tx => tx.UpdatedAddresses)
-                .ToImmutableHashSet();
-
-            var codec = new Codec();
-            foreach (var address in updatedAddresses)
-            {
-                var state = _chain.GetState(address, blockHash);
-                _publisher
-                    .SendMoreFrame(address.ToByteArray())
-                    .SendMoreFrame(NetworkOrderBitsConverter.GetBytes(e.Index))
-                    .SendMoreFrame(e.Hash.ToByteArray())
-                    .SendMoreFrame(e.PreviousHash?.ToByteArray() ?? new byte[0])
-                    .SendFrame(codec.Encode(state));
-            }
-        }
+        private ResponseSocket _server;
+        private NetMQPoller _poller;
 
         public Agent(BlockChain<T> blockChain, IStore store)
         {
             _store = store ?? throw new ArgumentNullException(nameof(store));
-
             _chain = blockChain ?? throw new ArgumentNullException(nameof(blockChain));
-            _chain.TipChanged += TipChangedHandler;
         }
 
-        public Task StartAsync(ushort port)
+        public void RunAsync(ushort port)
         {
-            _publisher = new PublisherSocket($"tcp://*:{port}");
-            _monitor = new NetMQMonitor(_publisher, "inproc://pub.inproc", SocketEvents.Accepted);
-            _monitor.Accepted += (sender, args) => { Console.WriteLine(args.Address); };
-            return _monitor.StartAsync();
+            _server = new ResponseSocket($"tcp://*:{port}");
+            _poller = new NetMQPoller { _server };
+
+            _server.ReceiveReady += ProcessMessage;
+            
+            _poller.RunAsync();
         }
 
         public void Dispose()
         {
-            _chain.TipChanged -= TipChangedHandler;
-            _publisher?.Close();
-            _monitor?.Stop();
+            _poller?.Dispose();
+            _server?.Dispose();
+        }
+
+        private void ProcessMessage(object sender, NetMQSocketEventArgs eventArgs)
+        {
+            NetMQMessage raw = eventArgs.Socket.ReceiveMultipartMessage();
+            NetMQSocket client = eventArgs.Socket;
+            Message message = Message.Parse(raw);
+            Message reply = null;
+            switch (message)
+            {
+                case GetBlock getBlock:
+                    var block = _chain[getBlock.BlockHash];
+                    var payload = block.Serialize();
+                    reply = new Block(payload);
+                    break;
+                case GetTip getTip:
+                    var tip = _chain.Tip;
+                    reply = new Tip(tip.Index, tip.Hash);
+                    break;
+                case GetState getState:
+                    var state = _chain.GetState(
+                        getState.Address,
+                        getState.BlockHash);
+                    var codec = new Codec();
+                    reply = new State(codec.Encode(state));
+                    break;
+                case GetBlockHash getBlockHash:
+                    var blockHash = _chain[getBlockHash.BlockIndex].Hash;
+                    reply = new BlockHash(blockHash);
+                    break;
+                default:
+                    throw new InvalidMessageException(
+                        $"Unhandled message [type: {message.GetType()}].");
+            }
+
+            client.SendMultipartMessage(reply.ToNetMQMessage());
         }
     }
 }
